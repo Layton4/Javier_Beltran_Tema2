f42c5083f67da05bf98aaee26b8914bc4434c6ed
Layton4 <Javierete4774@gmail.com>
2021-10-25 10:44:03 +0200

    Vectores y funciones

--- Tema2/Assets/Scripts/MyFirstScript.cs
+++ Tema2/Assets/Scripts/MyFirstScript.cs
@@ -17,10 +17,20 @@ public class MyFirstScript : MonoBehaviour
     public float a = 5f;
     public float b = 2f;
 
-    public bool isRaining = false;
-    */
+    
+   
 
     public int playerAge = 35;
+    public int x = 5;
+    public int y = 2;
+    public bool isRaining = false;
+    public bool isCold = false;
+    public int zi = 6%2;
+
+    [SerializeField] private int año;
+     */
+
+    [SerializeField] private int posicion1a8 = 0;
 
     void Start()
     {
@@ -48,7 +58,6 @@ public class MyFirstScript : MonoBehaviour
         {
             Debug.Log($"{x} es mayor que {y}");
         }
-        */
 
         if (playerAge >= 18)
         {
@@ -61,14 +70,158 @@ public class MyFirstScript : MonoBehaviour
         else
             Debug.Log("Eres un niño, como marco, en una humilde moradaaaa");
 
+        if (x == 2 && y <= 5) // la x vale 2 o la y es menor igual que 5
+        {
+            Debug.Log($"{x} tiene valor 2 y {y} es menor o igual que 5");
+        }
+        else if (x == 3 || y > 6) // x vale 3 o la y es mayor que 6
+        {
+            Debug.Log($"{x} tiene valor 3 o {y} es mayor que 2");
+        }
+        else if (x > 0 && y == 2) //x es mayor que 0 y la y vale 2
+        {
+            Debug.Log($"{x} es positivo y la variable y vale {y}");
+        }
+        else
+            Debug.Log("No es ningun caso previsto jajaja");
+         */
+        /*
+                if (isRaining)
+                {
+                    if (isCold)
+                    {
+                        Debug.Log("Lleva paraguas y sudadera");
+                    }
+                    else
+                    { 
+                        Debug.Log("Lleva paraguas");
+                    }
+                }
+                else
+                {
+                    if (isCold)
+                    {
+                        Debug.Log("Llevate una sudadera");
+                    }
+                    else
+                    {
+                        Debug.Log("No pases un buen día, pasa un gran día! :)");
+                    }
+                }
+
+                Debug.Log($"Estamos en el año {año}"); 
+
+                if (año%4 == 0) //divisible entre 4
+                {
+                    if (año%100 == 0) //Si es divisible entre 100 
+                    {
+                        if (año%400 == 0) //tiene que ser divisible entre 400
+                        {
+                            Debug.Log($"Este año es bisiesto");
+                        }
+                    }
+                    else //si no es divisible entre 100 pero si de 4
+                    {
+                        Debug.Log($"Este año es bisiesto");
+                    }
+
+                }
+                else //si ya no es divisible entre 4 ya no es bisiesto
+                {
+                    Debug.Log("No estamos en un año bisiesto");
+                }
+             
+
+        Debug.Log(transform.position);
+
+        transform.position = new Vector3(0,0,0);
+        if (posicion1a8 == 0)
+        {
+            transform.position = Vector3.zero;
+        }
+        if (posicion1a8 == 1)
+        {
+            transform.position = Vector3.one;
+        }
+        if (posicion1a8 == 2)
+        {
+            transform.position = Vector3.up;
+        }
+        if (posicion1a8 == 3)
+        {
+            transform.position = Vector3.down;
+        }
+        if (posicion1a8 == 4)
+        {
+            transform.position = Vector3.right;
+        }
+        if (posicion1a8 == 5)
+        {
+            transform.position = Vector3.left;
+        }
+        if (posicion1a8 == 6)
+        {
+            transform.position = Vector3.forward;
+        }
+        if (posicion1a8 == 7)
+        {
+            transform.position = Vector3.back;
+        }
+         */
 
 
     }
 
+
+
+
+
+
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(transform.position);
+        if (Input.GetKeyDown(KeyCode.D)) //Moverse a la derecha cuando pulsamos la flecha derecha
+        {
+            transform.position += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.A)) //Moverse a la izquierda cuando pulsamos la flecha izquierda
+        {
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.position += Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position += Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation *= Quaternion.Euler(0, 10, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation *= Quaternion.Euler(0, -10, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation *= Quaternion.Euler(10, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation *= Quaternion.Euler(-10, 0, 0);
+        }
         
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.localScale += Vector3.one;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            transform.localScale -= Vector3.one;
+        }
     }
 }
     
--- Tema2/Assets/Scripts/Vehiculo.cs
+++ Tema2/Assets/Scripts/Vehiculo.cs
@@ -10,42 +10,109 @@ public class Vehiculo : MonoBehaviour
     [SerializeField] private int numberWheels = 4; //aquí guardaremos el numero de ruedas del vehiculo
 
     [SerializeField] private bool hasSiren = false; // variable para indicar si tiene sirena
-    [SerializeField] private bool isOn = true;
- 
+    [SerializeField] private bool isOn = true; //esta variable indica si el vehiculo está en marcha
+
+    [SerializeField] private float gasoline;
+
+    public string hello;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-       /* Debug.Log($"{name} tiene {numberWheels} ruedas y hace {sound}");
+        /* Debug.Log($"{name} tiene {numberWheels} ruedas y hace {sound}");
 
-        if (hasSiren) //operador que comprueba si el vehiculo tiene sirena y lo indica en caso afirmativo
-        {
-            Debug.Log($"{name} tiene sirena");
-        
-        }
-        else
-        {
-            Debug.Log($"{name} no tiene sirena tonto, jaja salu2");
-        }*/
+         if (hasSiren) //operador que comprueba si el vehiculo tiene sirena y lo indica en caso afirmativo
+         {
+             Debug.Log($"{name} tiene sirena");
+         }
+         else
+         {
+             Debug.Log($"{name} no tiene sirena tonto, jaja salu2");
+         }
 
+         if (isOn) //si está en marcha el vehiculo
+         {
+             Debug.Log($"{sound}"); //muestra el sonido que hace el vehiculo
+         }
+         else if (hasSiren) //si no está en marcha pero tiene sirena
+         {
+             Debug.Log($"{name} hará {sound} cuando se ponga en marcha"); //nos avisa el sonido que hará al arrancar
+         }
+         else //en cualquier otro caso, o sea que no está en marcha y no tiene sirena
+         {
+             Debug.Log($"{name} no está en marcha"); //avisa que no está en marcha
+
+         }
+       
         if (isOn)
         {
-            Debug.Log($"{sound}");
-        }
-        else if (hasSiren)
-        {
-            Debug.Log($"{name} hará {sound} cuando se ponga en marcha");
+            if (gasoline < 10)
+            {
+                Debug.Log($"¡A {name} le queda poca gasofa, necesitamos repostar");
+            }
+            if (hasSiren)
+            {
+                Debug.Log(sound);
+            }
+            else
+            {
+                Debug.Log($"{name} no tiene sirena");
+            }
         }
         else
         {
             Debug.Log($"{name} no está en marcha");
+        
+         */
+        hello = GetHello();
 
-        }
+        HelloWorld();
+        //Debug.Log(hello);
+        Debug.Log(GetHello());
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (isOn)
+        {
+            if (Input.GetKeyDown(KeyCode.D)) //Moverse a la derecha cuando pulsamos la flecha derecha
+            {
+                transform.position += Vector3.right;
+            }
+            if (Input.GetKeyDown(KeyCode.A)) //Moverse a la izquierda cuando pulsamos la flecha izquierda
+            {
+                transform.position += Vector3.left;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position += Vector3.forward;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position += Vector3.back;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.rotation *= Quaternion.Euler(0, 10, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.rotation *= Quaternion.Euler(0, -10, 0);
+            }
+        }
+    }
+    public void HelloWorld()
+    {
+        Debug.Log("¡Hola mundo!");
+    }
+    public string GetHello()
+    {
+        return "Holi wapi";
     }
 }
